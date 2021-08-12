7b574bff1a8ae9819245183e1a2b434c13eff2b2
白洁 <460868559@qq.com>
2021-08-12 18:09:34 +0800

    add new

---
+++ src/main/java/com/rrif/pattern/factory/eazy_factory/AmericanCoffee.java
@@ -0,0 +1,13 @@
+
+package com.rrif.pattern.factory.eazy_factory;
+
+/**
+ *
+ */
+public class AmericanCoffee extends Coffee {
+
+    @Override
+    public String getName() {
+        return "American Coffee";
+    }
+}
---
+++ src/main/java/com/rrif/pattern/factory/eazy_factory/LatteCoffee.java
@@ -0,0 +1,12 @@
+
+package com.rrif.pattern.factory.eazy_factory;
+
+/**
+ *
+ */
+public class LatteCoffee extends Coffee {
+    @Override
+    public String getName() {
+        return "Latte Coffee";
+    }
+}
